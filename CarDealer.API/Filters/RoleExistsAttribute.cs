2ab8cb77d3287cec1598a176e75a71a860bb3e2d
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-07-05 20:16:57 +0300

    Role Attribute Added by Role

---
+++ CarDealer.API/Filters/RoleExistsAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealer.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CarDealer.API.Filters
+{
+    public class RoleExistsAttribute : TypeFilterAttribute
+    {
+        public RoleExistsAttribute() : base(typeof(RoleExistingFilter))
+        {
+
+        }
+
+        private class RoleExistingFilter : IAsyncActionFilter
+        {
+            private IRoleService roleService;
+
+            public RoleExistingFilter(IRoleService roleService)
+            {
+                this.roleService = roleService;
+            }
+            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            {
+                if (!context.ActionArguments.ContainsKey("id"))
+                {
+                    context.Result = new BadRequestResult();
+                    return;
+                }
+
+                if (!(context.ActionArguments["id"] is int id))
+                {
+                    context.Result = new BadRequestResult();
+                    return;
+                }
+
+                var category = roleService.GetRoleById(id);
+                if (category == null)
+                {
+                    context.Result = new NotFoundObjectResult(new { Message = $"{id} nolu rol bulunamadı." });
+                    return;
+                }
+
+                await next();
+            }
+        }
+    }
+}
